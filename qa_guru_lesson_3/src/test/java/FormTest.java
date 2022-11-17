142969ae74834b7fcc95d279155a697bfb2bf4b4
Boris Lysikov <38007083+BorisLys@users.noreply.github.com>
2022-11-17 16:31:21 +0700

    complete homework

---
+++ qa_guru_lesson_3/src/test/java/FormTest.java
@@ -0,0 +1,53 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.selector.ByText;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FormTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void fillFormTest() {
+        String userName = "Boris";
+        String lastName = "Lysikov";
+        String email = "blysikov@gmail.com";
+        String mobile = "89137775588";
+        String subjects = "English";
+        String currentAddress = "Novosibirsk";
+
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+        $("#firstName").setValue(userName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#gender-radio-1").doubleClick();
+        $("#userNumber").setValue(mobile);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2000");
+        $(".react-datepicker__day--020:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue(subjects).pressEnter();
+        $("#hobbiesWrapper").$(new ByText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("1.jpg");
+        $("#currentAddress").setValue(currentAddress);
+        $("#state").click();
+        $("#stateCity-wrapper").$(new ByText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(new ByText("Delhi")).click();
+        $("#submit").click();
+
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Boris Lysikov"), text(email), text("20 July,2000"), text("Male"), text(subjects), text(currentAddress));
+    }
+
+}
